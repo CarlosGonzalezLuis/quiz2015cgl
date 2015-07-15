55ac60b90b0f1154b080d6837ce265e8adc1533d
CarlosGonzalezLuis <cargonlu1@gmail.com>
2015-07-15 17:33:26 +0200

    presentacion autor

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,8 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/presentacion', function(req,res){
+            res.render('presentacion');
+});
 
 module.exports = router;
