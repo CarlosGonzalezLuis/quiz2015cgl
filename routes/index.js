6a0a345ad8cc29130f463980f91cc28997d1fbea
CarlosGonzalezLuis <cargonlu1@gmail.com>
2015-07-28 03:28:18 +0200

    Validacion con npm update

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 // Autoload de comandos con :quizId
@@ -19,7 +19,7 @@ router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
 
 router.get('/presentacion', function(req,res){
-            res.render('presentacion');
+            res.render('presentacion',{errors: []});
 });
 
 module.exports = router;
