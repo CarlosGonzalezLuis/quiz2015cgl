2590ac565244317cf231235689d9c299b417c5c5
CarlosGonzalezLuis <cargonlu1@gmail.com>
2015-08-04 00:53:01 +0200

    moderacion comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load); //autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login',   sessionController.new);       //formulario login
@@ -31,7 +32,8 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 //Definición de ruta comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments' ,   commentController.create);
-
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+                                       sessionController.loginRequired, commentController.publish);
 
 router.get('/presentacion', function(req,res){
             res.render('presentacion',{errors: []});
